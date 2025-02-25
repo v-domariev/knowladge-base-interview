b765b8b08f24817a3ef9ed666b59997d2c65f5df
Vladyslav Domariev <vlad.domariev@gmail.com>
2025-02-25 13:38:19 +0100

    Lambda as returning parameter

--- Code Practice/Code Practice/Delegate/Lambda3.cs
+++ Code Practice/Code Practice/Delegate/Lambda3.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Net.WebSockets;
 using System.Runtime.InteropServices.Marshalling;
@@ -26,13 +27,15 @@ namespace Code_Practice.Delegate
         public void Execute()
         {
             //Example1();
-            Example2ParametersListWithoutType();
-
+            //Example2ParametersListWithoutType();
+            //Example3ReturningValue();
+            Example5LambdaAsReturningParameter();
         }
         private void Example1()
         {
             var hello1 = () => Console.WriteLine("Hello"); // Due to "var" compiler is looking for delegate "Action".
-            var hello2 = () => { 
+            var hello2 = () =>
+            {
                 Console.WriteLine("Hello2");
             };
             hello1.Invoke();
@@ -43,7 +46,9 @@ namespace Code_Practice.Delegate
         }
 
         delegate void Operation(int x, int y);
+        delegate int OperationReturningValue(int x, int y);
         delegate void MessageHandler(string message);
+        delegate void MessageHandlerWithDefault(string message = "DEFAULT_IN_DELEGATE");
         private void Example2ParametersListWithoutType()
         {
             Operation sum = (x, y) =>
@@ -70,8 +75,122 @@ namespace Code_Practice.Delegate
 
 
             MessageHandler messageHander = message => Console.WriteLine(message);
-            messageHander
+            messageHander.Invoke("Message параметр без типа данных");
 
+            MessageHandlerWithDefault messageHandlerWithDefault = (string message) => Console.WriteLine(message);
+            var lambdaWithDefault = (string message = "DEFAULT_MESSAGE_IN_LAMBDA") => Console.WriteLine(message);
+            messageHandlerWithDefault.Invoke();
+            lambdaWithDefault.Invoke();
         }
+
+        // A lambda expression can return a result.
+        private void Example3ReturningValue()
+        {
+            var sum = (int x, int y) => x + y;
+            int sumResult = sum(4, 5);
+            Console.WriteLine(sumResult);
+            OperationReturningValue multiply = (x, y) => x * y;
+            int multiplyResult = multiply(4, 5);
+            Console.WriteLine(multiplyResult);
+
+            var substract = (int x, int y) =>
+            {
+                if (x > y)
+                {
+                    return x - y;
+                }
+                else
+                {
+                    return y - x;
+                }
+
+            };
+            int result1 = substract.Invoke(10, 6);
+            int result2 = substract.Invoke(-10, 6);
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+        }
+
+        private void Example4AddingRemovingFromInvokeList()
+        {
+            var hello = () => Console.WriteLine("METANIT.COM");
+            var message = () => Console.WriteLine("Hello ");
+            message += () => Console.WriteLine("World");
+            message += hello;
+            message += Print;
+
+            message?.Invoke();
+
+            Console.WriteLine("-----------------------");
+
+            message -= Print;
+            message -= hello;
+
+            message?.Invoke();
+
+            void Print() => Console.WriteLine("Welcome to C#");
+        }
+
+        delegate bool IsEqual(int x);
+        private void Example5LambdaAsMethodParameter()
+        {
+            int[] integers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int result1 = Sum(integers, x => x > 5);
+            Console.WriteLine(result1);
+            int result2 = Sum(integers, x => x % 2 == 0);
+            Console.WriteLine(result2);
+
+
+            int Sum(int[] numbers, IsEqual func)
+            {
+                int result = 0;
+                foreach (int x in numbers)
+                {
+                    if (func(x))
+                    {
+                        result += x;
+                    }
+                }
+                return result;
+            }
+        }
+
+
+        enum EmpType
+        {
+            Admin,
+            Operator,
+            Cashier
+        }
+
+        delegate MessageHandlerWithDefault WhichEmpType(EmpType empType);
+
+        private void Example5LambdaAsReturningParameter()
+        {
+
+            WhichEmpType whichEmpType = (EmpType empType) =>
+            {
+                MessageHandlerWithDefault messageHandlerWithDefault = (string msg) => Console.WriteLine($"SWITCH_HAVE_NO_MATCHES. {msg}");
+                switch (empType)
+                {
+                    case EmpType.Admin:
+                        messageHandlerWithDefault = (string msg) => Console.WriteLine($"I am an admin. {msg}");
+                        break;
+                    case EmpType.Operator:
+                        messageHandlerWithDefault = (string msg) => Console.WriteLine($"I am an Operator. {msg}");
+                        break;
+                    case EmpType.Cashier:
+                        messageHandlerWithDefault = (string msg) => Console.WriteLine($"I am an Cashier. {msg}");
+                        break;
+                }
+
+                return messageHandlerWithDefault;
+            };
+
+            whichEmpType.Invoke(EmpType.Cashier)?.Invoke("Cashier type send");
+            whichEmpType.Invoke(EmpType.Operator)?.Invoke("Operator type send");
+            whichEmpType.Invoke(EmpType.Admin)?.Invoke("Admin type send");
+        }
+
     }
 }
